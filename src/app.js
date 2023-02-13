2c6dc17813b6669f238723952fcbfbe5f13cf794
Hon07 <hangulo14@gmail.com>
2023-02-13 05:31:45 +0000

    hello

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pro = ["best", "smart", "happy"];
+  let adj = ["big", "modern", "creative"];
+  let noun = ["sun", "cloud", "sky"];
+  let dom = [".com", ".net", ".org"];
+  for (let a = 0; a < pro.length; a++) {
+    for (let b = 0; b < adj.length; b++) {
+      for (let c = 0; c < noun.length; c++) {
+        for (let d = 0; d < dom.length; d++) {
+          domainName.push(" " + pro[a] + adj[b] + noun[c] + dom[d]);
+        }
+      }
+    }
+  }
+  console.log("Any string");
+  document.getElementById("domain").innerHTML = domainName;
 };
